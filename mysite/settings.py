8eaa9d943063d2639059302b6a136b5591b11ec9
Noly Espardinez <espardin@gmail.com>
2023-02-21 08:27:07 +0800

    second update

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,13 +78,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'project1',
-        'USER': 'postgres',
-        'PASSWORD': 'nolcubs03',
-        'HOST': 'localhost',    
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
